97aa118a5420dbec5b0cfa9f155111ac5b99513c
kblache <72400888+kblache@users.noreply.github.com>
2021-02-18 22:59:50 +0100

    maj

--- TestEtexamplesCours/WhyUsePointers/WhyUsePointers.cpp
+++ TestEtexamplesCours/WhyUsePointers/WhyUsePointers.cpp
@@ -8,8 +8,7 @@ using std::endl;
 int main()
 {
 	const char* teachers[] = { "Brice", "Manuel","Gaetan","Mathieu","Guillaume" };
-	const char** 3dTeachers = { "Marja","Eric","Jerome","Mathieu","Antonin" };
+	const char** 3dTeachers = { "Marja", "Eric","Jerome","Mathieu", "Antonin" };
 	const int* age = { 34, 28, 33, 35, 34 }; 
-
 
 }
